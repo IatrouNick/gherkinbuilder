885f4aecef7b6ffc43fbe877dc0fbf55b910cb7e
IatrouNick <IatrouNick@users.noreply.github.com>
2025-07-22 18:54:06 +0000

    deploy: 695d438f4e376bf077b9809982977031929a02d7

---
+++ js/Download.js
@@ -0,0 +1,9 @@
+﻿window.downloadFile = (filename, content) => {
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = filename;
+    anchor.click();
+    URL.revokeObjectURL(url);
+};
