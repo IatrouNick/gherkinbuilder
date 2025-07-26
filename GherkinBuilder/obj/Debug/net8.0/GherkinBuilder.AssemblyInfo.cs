a247e244b2b2a5bf1c840c17bbf176ec49cdd550
IatrouNick <iatrounick@gmail.com>
2025-07-26 17:03:40 +0300

    added locators to elements

--- GherkinBuilder/obj/Debug/net8.0/GherkinBuilder.AssemblyInfo.cs
+++ GherkinBuilder/obj/Debug/net8.0/GherkinBuilder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GherkinBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+695d438f4e376bf077b9809982977031929a02d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+454ae68d25670dbf48d6303a59712d43be145b05")]
 [assembly: System.Reflection.AssemblyProductAttribute("GherkinBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GherkinBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
